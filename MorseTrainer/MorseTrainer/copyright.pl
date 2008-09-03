faa7945b825c47e002e507f7932fe6b80c3f8b69
nall <nall@983005e0-a9c2-4b13-9297-b893369d9406>
2008-09-03 05:43:24 +0000

    - Fixed license notice to not refer to "Foobar" - NO functional changes

--- MorseTrainer/MorseTrainer/copyright.pl
+++ MorseTrainer/MorseTrainer/copyright.pl
@@ -28,13 +28,13 @@ print OUT <<EOH;
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
 // 
-// Foobar is distributed in the hope that it will be useful,
+// AD5RX Morse Trainer is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 // 
 // You should have received a copy of the GNU General Public License
-// along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+// along with AD5RX Morse Trainer.  If not, see <http://www.gnu.org/licenses/>.
 
 EOH
 
