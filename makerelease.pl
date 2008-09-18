3295eb5e275da7e5631a8bcb06c3c9da51302ecc
nall <nall@983005e0-a9c2-4b13-9297-b893369d9406>
2008-09-18 01:03:36 +0000

    - release helper script

---
+++ makerelease.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+use strict;
+
+my $CP = "/bin/cp";
+my $RM = "/bin/rm";
+my $MKDIR = "/bin/mkdir";
+my $HDIUTIL = "/usr/bin/hdiutil";
+my $VERSION = "1.0";
+
+my $RELEASEDIR = "/tmp/AD5RX\\ Morse\\ Trainer";
+
+my @RELEASEFILES = (
+                        "COPYING",
+                        "NoiseLicense.pdf",
+                        "SparkleLicense.txt",
+                        "Word\\ Files",
+                        "QSO\\ Files",
+                        "build/Release/MorseTrainer.app"
+                   );
+
+&main();
+
+sub main()
+{
+    my $pwd = `pwd`;
+    chomp($pwd);
+
+    system("$RM -rf $RELEASEDIR");
+    system("$MKDIR -p $RELEASEDIR");
+
+    my $dmgimage = "$pwd/AD5RXMorseTrainer-$VERSION.dmg";
+    
+    foreach my $file (@RELEASEFILES)
+    {
+        system("$CP -R $file $RELEASEDIR");
+    }
+
+    system("$HDIUTIL create $dmgimage -srcfolder $RELEASEDIR");
+
+    system("$RM -rf $RELEASEDIR");
+}
+
